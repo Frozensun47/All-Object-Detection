e4c2bcbfa365ee6f36237b06778347b294bb48c3
Frozensun47 <sagar.srivastava.cd.mec20@itbhu.ac.in>
2023-12-23 22:41:03 +0530

    readded code to save images

--- client.py
+++ client.py
@@ -5,13 +5,6 @@ from PIL import Image
 import io
 import base64
 
-# Get secrets from Streamlit Secrets
-secrets = st.secrets
-
-# Get server URL and port from secrets
-server_url =  'http://10.12.228.206'#secrets['server_url']
-server_port = 5432 #secrets['server_port']
-
 st.title('Image Upload Client')
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
@@ -20,28 +13,24 @@ if uploaded_file:
     st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
     st.write("")
 
-    # Add "Send" and "Clear" buttons
-    col1, col2 = st.columns(2)
-    if col1.button("Send"):
+    # Add a "Send" button
+    if st.button("Send"):
         st.write("Sending image to server...")
-
+        
         # Send image to server
-        url = f'{server_url}:{server_port}/inbound'
+        url = 'http://10.12.173.109:5432/inbound'
         files = {'file': uploaded_file}
         response = requests.post(url, files=files)
-
+        
         if response.status_code == 200:
             st.success("Image successfully sent to server and saved.")
-
+            
             # Get the base64 image from the response
-            base64_image = response.json().get('result_image_base64')
-
-            if base64_image:
-                # Display the base64 image in Streamlit
-                image = Image.open(io.BytesIO(base64.b64decode(base64_image)))
-                st.image(image, caption="Processed Image", use_column_width=True)
-
-    # Loading bar for indication
-    with st.spinner("Processing..."):
-        import time
-        time.sleep(3)  # Simulating processing time
+            base64_image = response.json()['result_image_base64']
+            
+            # Display the base64 image in Streamlit
+            image = Image.open(io.BytesIO(base64.b64decode(base64_image)))
+            st.image(image, caption="Processed Image", use_column_width=True)
+            
+        else:
+            st.error(f"Error sending image to server. Status code: {response.status_code}")
--- server.py
+++ server.py
@@ -40,6 +40,11 @@ if st.button("Start Server"):
             filename = secure_filename(file.filename)
             st.write(f"File '{filename}' received and saved.")
             st.info(f"Processing image: {filename}")
+
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+            image_file = plt.imread(file_path)
             buffer = detect_objects(file)
             face_base64 = base64.b64encode(buffer).decode('utf-8')
 
